2ceb42a1e14b67712bf3e446d70740595d316859
Hao Wen <wenhao@126.com>
2015-09-30 14:24:18 +0800

    [Wen Hao] - config spring-integration-kafka.

--- spring-kafka/src/main/java/org/github/wenhao/kafka/api/ProducerApi.java
+++ spring-kafka/src/main/java/org/github/wenhao/kafka/api/ProducerApi.java
@@ -3,6 +3,8 @@ package org.github.wenhao.kafka.api;
 import static org.springframework.web.bind.annotation.RequestMethod.POST;
 
 import org.github.wenhao.kafka.domain.User;
+import org.github.wenhao.kafka.service.ProducerService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -12,8 +14,16 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping(value = "/producer")
 public class ProducerApi {
 
+    private ProducerService producerService;
+
+    @Autowired
+    public ProducerApi(ProducerService producerService) {
+        this.producerService = producerService;
+    }
+
     @RequestMapping(method = POST)
     public ResponseEntity produce(@RequestBody User user) {
-        return ResponseEntity.ok().build();
+        producerService.produce(user);
+        return ResponseEntity.ok(user);
     }
 }
--- spring-kafka/src/main/java/org/github/wenhao/kafka/service/ProducerService.java
+++ spring-kafka/src/main/java/org/github/wenhao/kafka/service/ProducerService.java
@@ -1,7 +1,29 @@
 package org.github.wenhao.kafka.service;
 
+import org.github.wenhao.kafka.domain.User;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.integration.kafka.support.KafkaHeaders;
+import org.springframework.integration.support.MessageBuilder;
+import org.springframework.messaging.Message;
+import org.springframework.messaging.MessageChannel;
 import org.springframework.stereotype.Service;
 
 @Service
 public class ProducerService {
+
+    private MessageChannel inputToKafka;
+
+    @Autowired
+    public ProducerService(MessageChannel inputToKafka) {
+        this.inputToKafka = inputToKafka;
+    }
+
+    public void produce(User user) {
+        Message<User> message = MessageBuilder.withPayload(user)
+                .setHeader(KafkaHeaders.MESSAGE_KEY, "user")
+                .setHeader(KafkaHeaders.TOPIC, "test")
+                .build();
+        inputToKafka.send(message);
+    }
+
 }
