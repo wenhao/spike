8e017d9bcdee0a8452dbbeab960438606de51e1b
Hao Wen <wenhao@126.com>
2015-11-09 13:17:07 +0800

    [Wen Hao] - add init db script.

--- spring-quartz/src/main/java/com/github/wenhao/quartz/config/QuartzConfig.java
+++ spring-quartz/src/main/java/com/github/wenhao/quartz/config/QuartzConfig.java
@@ -3,7 +3,6 @@ package com.github.wenhao.quartz.config;
 
 import javax.sql.DataSource;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.core.io.ClassPathResource;
@@ -12,11 +11,8 @@ import org.springframework.scheduling.quartz.SchedulerFactoryBean;
 @Configuration
 public class QuartzConfig {
 
-    @Autowired
-    private DataSource dataSource;
-
     @Bean
-    public SchedulerFactoryBean schedulerFactoryBean() {
+    public SchedulerFactoryBean schedulerFactoryBean(DataSource dataSource) {
         SchedulerFactoryBean bean = new SchedulerFactoryBean();
         bean.setDataSource(dataSource);
         bean.setConfigLocation(new ClassPathResource("/quartz.properties"));
