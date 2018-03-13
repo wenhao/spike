e0a938973099c53b041ce1ff2ff2f59c862d05d6
Hao Wen <wenhao@126.com>
2018-03-13 15:32:27 +0800

    [Wen Hao] - add spring config server.

--- spring-cloud/spring-cloud-config-client/src/main/java/com/github/wenhao/config/ApplicationConfig.java
+++ spring-cloud/spring-cloud-config-client/src/main/java/com/github/wenhao/config/ApplicationConfig.java
@@ -1,11 +1,13 @@
 package com.github.wenhao.config;
 
 import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.validation.annotation.Validated;
 
 import javax.validation.constraints.NotBlank;
 
+@RefreshScope
 @Configuration
 @ConfigurationProperties(prefix = "application.kubernetes")
 @Validated
