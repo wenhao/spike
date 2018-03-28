a9c128f8ceddeceec9e867f1e77a7aaae677469f
Hao Wen <wenhao@126.com>
2018-03-28 11:04:14 +0800

    [Wen Hao] - add spring config server.

--- spring-cloud/spring-cloud-config-client/src/main/java/com/github/wenhao/controller/SpringConfigClientController.java
+++ spring-cloud/spring-cloud-config-client/src/main/java/com/github/wenhao/controller/SpringConfigClientController.java
@@ -1,7 +1,6 @@
 package com.github.wenhao.controller;
 
 import com.github.wenhao.config.ApplicationConfig;
-import com.github.wenhao.config.SpringConfig;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -10,18 +9,11 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class SpringConfigClientController {
 
-    private SpringConfig springConfig;
     private ApplicationConfig applicationConfig;
 
     @Autowired
-    public SpringConfigClientController(SpringConfig springConfig, ApplicationConfig applicationConfig) {
-        this.springConfig = springConfig;
+    public SpringConfigClientController(ApplicationConfig applicationConfig) {
         this.applicationConfig = applicationConfig;
-    }
-
-    @GetMapping(path = "/account")
-    public ResponseEntity get() {
-        return ResponseEntity.ok(String.format("username and password: %s:%s", springConfig.getUsername(), springConfig.getPassword()));
     }
 
     @GetMapping(path = "/kubernetes")
