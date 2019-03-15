da8d0441f2bbc1de64fa6ea8f68ff86fdabfae0c
Wen Hao <wenhao@126.com>
2019-03-15 14:32:24 +0800

    [TECH][wenhao] add terraform.

---
+++ terraform/example.tf
@@ -0,0 +1,27 @@
+provider "aws" {
+  access_key = "${var.access_key}"
+  secret_key = "${var.secret_key}"
+  region = "${var.region}"
+}
+
+resource "aws_instance" "example" {
+  ami = "ami-061392db613a6357b"
+  instance_type = "t2.micro"
+
+  depends_on = ["aws_s3_bucket.example"]
+
+  provisioner "local-exec" {
+      command = "echo ${aws_instance.example.public_ip} > ip_address.txt"
+  }
+}
+
+resource "aws_eip" "ip" {
+  instance = "${aws_instance.example.id}"
+}
+
+resource "aws_s3_bucket" "example" {
+  bucket = "terraform-wenhao-getting-started-guide"
+  acl = "private"
+}
+
+
---
+++ terraform/terraform.tfvars
@@ -0,0 +1,2 @@
+access_key = "AKIAI425DMJP72CZFL7A"
+secret_key = "e6Hka3yaONrCCZJTKlKvy3TpqavKAtJVQGDgo/jU"
