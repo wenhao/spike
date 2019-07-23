5e53478485fd723c7d848137fe40277db69c4fbf
Wen Hao <wenhao@126.com>
2019-07-23 15:49:01 +0800

    [TECH][wenhao] update k8s config.

---
+++ spring-cloud/spring-cloud-eureka-client/src/main/java/com/github/wenhao/client/EurekaServiceClient.java
@@ -0,0 +1,12 @@
+package com.github.wenhao.client;
+
+import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+@FeignClient(name = "SPRING-CLOUD-EUREKA-SERVICE")
+public interface EurekaServiceClient {
+
+    @RequestMapping(value = "services", method = RequestMethod.GET)
+    String get();
+}
--- spring-cloud/spring-cloud-eureka-client/src/main/java/com/github/wenhao/config/BeanConfig.java
+++ spring-cloud/spring-cloud-eureka-client/src/main/java/com/github/wenhao/config/BeanConfig.java
@@ -1,16 +1,8 @@
 package com.github.wenhao.config;
 
-import org.springframework.cloud.client.loadbalancer.LoadBalanced;
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.web.client.RestTemplate;
 
 @Configuration
 public class BeanConfig {
 
-    @Bean
-    @LoadBalanced
-    public RestTemplate restTemplate() {
-        return new RestTemplate();
-    }
 }
