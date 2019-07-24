5aaa80a1089cac4e50c4da7b41dab3c51623689f
Wen Hao <wenhao@126.com>
2019-07-24 15:24:38 +0800

    [TECH][wenhao] update config.

--- spring-cloud/spring-cloud-service-consumer/src/main/java/com/github/wenhao/client/EurekaServiceClient.java
+++ spring-cloud/spring-cloud-service-consumer/src/main/java/com/github/wenhao/client/EurekaServiceClient.java
@@ -4,7 +4,7 @@ import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
-@FeignClient(name = "SPRING-CLOUD-EUREKA-SERVICE")
+@FeignClient(name = "SPRING-CLOUD-SERVICE-PROVIDER")
 public interface EurekaServiceClient {
 
     @RequestMapping(value = "services", method = RequestMethod.GET)
